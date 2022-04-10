bcdc96f7ee8e8042d995b5cb7d2cad7a96fe01be
Zhen Cai <zhen.cai@salesforce.com>
2022-04-10 00:00:51 -0700

    update key

--- src/main/java/com/cenylcz/jpa/repository/WordRepository.java
+++ src/main/java/com/cenylcz/jpa/repository/WordRepository.java
@@ -3,5 +3,5 @@ package com.cenylcz.jpa.repository;
 import com.cenylcz.domain.dictionary.Word;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WordRepository extends JpaRepository<Word, Integer> {
+public interface WordRepository extends JpaRepository<Word, String> {
 }
